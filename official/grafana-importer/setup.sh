b474de4ab04e103aa19a5ea4f5eaf6fe47cc845a
Yufei Li <yufeiminds@gmail.com>
2023-09-04 12:58:35 +0800

    fix: Fix setup.sh

--- official/grafana-importer/setup.sh
+++ official/grafana-importer/setup.sh
@@ -7,8 +7,3 @@ curl -O https://releases.hashicorp.com/terraform/"$TF_VERSION"/terraform_"$TF_VE
 unzip terraform_"$TF_VERSION"_linux_amd64.zip -d /usr/local/bin/
 rm terraform_"$TF_VERSION"_linux_amd64.zip
 
-# Install Guance CLI
-echo "deb [trusted=yes] https://releases.guance.io/apt/ /" | sudo tee /etc/apt/sources.list.d/guance.list
-sudo apt update
-sudo apt install guance
-
